790c705c91a698ec4c71195a81c139dc658a9c39
Claudio Jolowicz <cj@dealloc.org>
2011-03-06 18:00:00 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdemo::Application.config.secret_token = 'b5e34813d00acb873dcd53c8b2c2426fc3a5673e7e82805e6852a1cacef22a0e2aea35c8e87a670df81403da45a8097c73f273a5cc32985144f38f235cf2a55b'
